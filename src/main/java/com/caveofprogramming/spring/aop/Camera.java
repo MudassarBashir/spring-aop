40c375d7097ecce06f2db6540badca2b53f15049
Moe B <moeb59@gmail.com>
2016-12-13 21:40:47 -0500

    Vid 77: Annotation based aspects.

--- src/main/java/com/caveofprogramming/spring/aop/Camera.java
+++ src/main/java/com/caveofprogramming/spring/aop/Camera.java
@@ -1,8 +1,11 @@
 package com.caveofprogramming.spring.aop;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Moe B. on 12/13/2016.
  */
+@Component
 public class Camera {
 
     public void snap() {
--- src/main/java/com/caveofprogramming/spring/aop/Logger.java
+++ src/main/java/com/caveofprogramming/spring/aop/Logger.java
@@ -1,5 +1,9 @@
 package com.caveofprogramming.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Moe B. on 12/13/2016.
  */
@@ -7,8 +11,11 @@ package com.caveofprogramming.spring.aop;
 In the next video, this class gets turned into a Spring Aspect which will advise the camera
 class.
  */
+@Aspect
+@Component
 public class Logger {
 
+    @Before("execution(void com.caveofprogramming.spring.aop.Camera.snap())")
     public void aboutToTakePhoto() {
         System.out.println("About to take photo...");
     }
